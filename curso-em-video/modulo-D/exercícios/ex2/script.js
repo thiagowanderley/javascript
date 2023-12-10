8fb87bb164ded69bc86e27876e731cc5ad252913
Thiago Wanderley <146901800+thiagowanderley@users.noreply.github.com>
2023-12-10 01:55:40 -0300

    Continuação do ex 03

--- curso-em-video/modulo-D/exercícios/ex2/script.js
+++ curso-em-video/modulo-D/exercícios/ex2/script.js
@@ -1,13 +1,49 @@
-btn.addEventListener('click', clicar)
+btn.addEventListener('click', verificar)
 
-function clicar() {
-    var nasc = document.getElementById('nasc')
-    var n = Number(nasc.value)
-    var idade = 2024 - n
-    var masculino = document.getElementById('masculino')
-    var feminino = document.getElementById('feminino')
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
     var btn = document.getElementById('btn')
-    var resultado = document.getElementById('resultado')
+    var nasc = document.getElementById('nasc')
+    var res = document.getElementById('res')
+    var img = document.getElementById('img')
 
-    resultado.innerHTML = `<p> Você tem${idade} anos</p>`
+    if (nasc.value.length == 0 || nasc.value > ano) {
+        window.alert('Verifique os dados e digite novamente [ERRO]')
+    } else {
+        var fsex = document.getElementsByName('sexo')
+        var idade = ano - Number(nasc.value)
+        var genero = ''
+        
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade < 13) {
+                //criança
+                img.getAttribute = ('src', 'criancam.jpg')
+            } else if (idade >= 13 && idade < 21) {
+                //adolescente
+                img.getAttribute = ('src', 'adolescentem.jpg')
+            } else if (idade >=21 && idade < 61) {
+                //adulto
+                img.getAttribute = ('src', 'adulto.jpg')
+            } else if (idade >= 61) {
+                //idoso
+                img.getAttribute = ('src', 'idoso.jpg')
+            }
+
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 13) {
+                //criança
+            } else if (idade >= 13 && idade < 21) {
+                //adolescente
+            } else if (idade >=21 && idade < 61) {
+                //adulto
+            } else if (idade >= 61) {
+                //idoso
+            }
+        }
+        res.innerText = `Detectamos um(a) ${genero} com ${idade} anos`
+        res.appendChild(img)
+    }
 }

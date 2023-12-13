6ef80aee0c55e89fdc82a3c32f5aeea30fab7013
Thiago Wanderley <146901800+thiagowanderley@users.noreply.github.com>
2023-12-13 00:43:29 -0300

    Reformulação e Finalização
    Reformulação do script do ultimo ex do modulo D e consequentemente, o seu fim.

--- curso-em-video/modulo-D/exercícios/ex2/script.js
+++ curso-em-video/modulo-D/exercícios/ex2/script.js
@@ -3,47 +3,58 @@ btn.addEventListener('click', verificar)
 function verificar() {
     var data = new Date()
     var ano = data.getFullYear()
-    var btn = document.getElementById('btn')
-    var nasc = document.getElementById('nasc')
+    var fano = document.getElementById('nasc')
     var res = document.getElementById('res')
-    var img = document.getElementById('img')
+    var fsex = document.getElementsByName('sexo') //definindo a variavel do form(radio) -- [0]M e [1]F
+    var idade = ano - Number(fano.value)
+    var genero = ''
+    var foto = document.querySelector('img#foto')
+    //var img = document.createElement('img')
+    //img.setAttribute('id', 'foto')
+    
 
-    if (nasc.value.length == 0 || nasc.value > ano) {
-        window.alert('Verifique os dados e digite novamente [ERRO]')
-    } else {
-        var fsex = document.getElementsByName('sexo')
-        var idade = ano - Number(nasc.value)
-        var genero = ''
-        
-        if (fsex[0].checked) {
-            genero = 'Homem'
-            if (idade >= 0 && idade < 13) {
-                //criança
-                img.getAttribute = ('src', 'criancam.jpg')
-            } else if (idade >= 13 && idade < 21) {
-                //adolescente
-                img.getAttribute = ('src', 'adolescentem.jpg')
-            } else if (idade >=21 && idade < 61) {
-                //adulto
-                img.getAttribute = ('src', 'adulto.jpg')
-            } else if (idade >= 61) {
-                //idoso
-                img.getAttribute = ('src', 'idoso.jpg')
-            }
+    if (fano.value == 0 || fano.value > ano) {
+        alert('Verifique os dados e faça novamente')
+        res.innerHTML = 'INVALIDO'
 
-        } else if (fsex[1].checked) {
-            genero = 'Mulher'
-            if (idade >= 0 && idade < 13) {
-                //criança
-            } else if (idade >= 13 && idade < 21) {
-                //adolescente
-            } else if (idade >=21 && idade < 61) {
-                //adulto
-            } else if (idade >= 61) {
-                //idoso
-            }
+    } else if (fsex[0].checked) {
+        var genero = 'Homem'
+
+        if (idade >= 0 && idade < 12) {
+            foto.src = 'imagens/criancam.jpg'
+
+        } else if (idade < 21) {
+            foto.src = 'imagens/adolescentem.jpg'
+
+        } else if (idade < 60) {
+            foto.src = 'imagens/adulto.jpg'
+
+        } else {
+            foto.src = 'imagens/idoso.jpg'
         }
-        res.innerText = `Detectamos um(a) ${genero} com ${idade} anos`
-        res.appendChild(img)
+
+
+    } else if (fsex[1].checked) {
+        var genero = 'Mulher'
+
+        if (idade >= 0 && idade < 12) {
+            foto.src = 'imagens/criancaf.jpg'
+
+        } else if (idade < 21) {
+            foto.src = 'imagens/adolescentef.jpg'
+
+        } else if (idade < 60) {
+            foto.src = 'imagens/adulta.jpg'
+
+        } else {
+            foto.src = 'imagens/idosa.jpg'
+        }
+
     }
+     
+    foto.style.display = 'block'
+    foto.style.margin = 'auto'
+    res.innerHTML = `detectamos um(a) ${genero} com ${idade} anos`
+    res.appendChild(foto)
+    
 }

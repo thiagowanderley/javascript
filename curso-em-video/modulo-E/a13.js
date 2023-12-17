eaf2e253a65e86b409071514d6f4116e7a2f60f4
Thiago Wanderley <146901800+thiagowanderley@users.noreply.github.com>
2023-12-16 22:29:33 -0300

    finalizacao da aulas do modulo E

---
+++ curso-em-video/modulo-E/a13.js
@@ -0,0 +1,14 @@
+var c = 1
+
+do { // do = faca
+    console.log(`passo ${c}`)
+    c++
+} while(c <= 7) // while = enquanto
+
+
+/* 
+while (c <= 6) {
+    console.log('Ola')
+    c++ // c = c + 1
+}
+*/

b63bed4cd2c3b09a3fe8c893ec14b804c5dbea31
Thiago Wanderley <146901800+thiagowanderley@users.noreply.github.com>
2023-12-17 16:43:33 -0300

    Finalizacao do ex1

---
+++ curso-em-video/modulo-E/exercícios/ex01/script.js
@@ -0,0 +1,27 @@
+function contar() {
+    var res = document.getElementById('res')
+    var ini = document.getElementById('ini')
+    var fim = document.getElementById('fim')
+    var pas = document.getElementById('pas')
+    var i = Number(ini.value)
+    var f = Number(fim.value)
+    var p = Number(pas.value)
+    var c = i
+
+    if (i.length == 0 || f.length == 0 || p.length == 0) {
+        alert('Preencha todos os campos')
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        
+        if (p <= 0) {
+            alert('Valor de Passo invalido. Considerando valor 1')
+            p = 1
+        }
+
+        if (i < f) {
+            for(c = i; c += p; c <= f)
+            res.innerHTML += `${c}`
+        }
+    }
+
+}

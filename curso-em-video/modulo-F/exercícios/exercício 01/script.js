d32f98c4743e988f013c3f280eb4af68b118421b
Thiago Wanderley <146901800+thiagowanderley@users.noreply.github.com>
2024-03-19 16:51:12 -0300

    Realização parcial do primeiro ex do modulo F

---
+++ curso-em-video/modulo-F/exercícios/exercício 01/script.js
@@ -0,0 +1,9 @@
+var num = document.getElementById('fnum')
+var lista = document.getElementById('flista')
+var res = document.getElementById('res')
+var valores = []
+
+
+function adicionar() {
+    if(isNumero(num.value) &&)
+}

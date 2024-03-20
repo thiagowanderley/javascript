7457d2133629328cb1eeb90541cb2b54e46c98ae
Thiago Wanderley <146901800+thiagowanderley@users.noreply.github.com>
2024-03-20 13:09:50 -0300

    Inicio do curso da plataforma da udemy

---
+++ udemy/aula03/index.js
@@ -0,0 +1,14 @@
+//String = Texto - Dentro de aspas ou crases
+console.log('Ola')
+console.log("mundo")
+console.log(`Ola Mundo!`)
+console.log('Thiago "Wanderley"')
+console.log("Thiago 'Wanderley'")
+console.log(`'Thiago' "Wanderley"`)
+
+//Number = Numero - Não precisa de crases
+console.log(25)
+console.log(25.89)
+
+//string + number
+console.log('Nome: Thiago; Idade:', 23)

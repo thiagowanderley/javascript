7567d99e7bd9b1f58a14e8cfec15d2a37417fcad
Thiago Wanderley <146901800+thiagowanderley@users.noreply.github.com>
2024-03-20 10:52:16 -0300

    Exercício incompleto
    Devido ao tempo que fiquei parado, eu não consegui acompanha o raciocínio do professor. Por conta disso, vou recomeçar do zero : (

--- curso-em-video/modulo-F/exercícios/exercício 01/script.js
+++ curso-em-video/modulo-F/exercícios/exercício 01/script.js
@@ -3,7 +3,27 @@ var lista = document.getElementById('flista')
 var res = document.getElementById('res')
 var valores = []
 
+function isNumero(n) {
+    if(Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function inLista(n, l) {
+    if (l.indexof(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+} 
+
 
 function adicionar() {
-    if(isNumero(num.value) &&)
+    if(isNumero(num.value) && !inLista(num.value, valores)) {
+        valores.push(Number(num.value))
+    } else {
+        window.alert('valor invalido ou ja encontrado na lista')
+    }
 }

f7716feca7b6051475c35caa31cb2bd329addaf6
Thiago Wanderley <146901800+thiagowanderley@users.noreply.github.com>
2024-04-24 12:50:13 -0300

    Criei o exercicio com a minha logica

---
+++ udemy/aula48/assets/js/script2.js
@@ -0,0 +1,46 @@
+function meuEscopo () {
+
+const formulario = document.getElementById('formulario');
+const inputTarefas = document.getElementById('input-nova-tarefa');
+const ulTarefas = document.getElementById('tarefas');
+
+
+formulario.addEventListener('submit', function enviarForm (e) {
+    e.preventDefault();
+    expressarNaTela();
+    reniciarInput();
+    criarArray();
+})
+
+function expressarNaTela () {
+    if (!inputTarefas.value) return //não cria lista caso o input esteja vazio
+    const li = document.createElement('li');
+    li.setAttribute('class', 'elemento-lista');
+    li.innerText = inputTarefas.value;
+    ulTarefas.appendChild(li);
+    criarBotaoApagar(li);
+}
+
+function reniciarInput () {
+    inputTarefas.value = '';
+    inputTarefas.focus();
+}
+
+function criarBotaoApagar (li) {
+    li.innerText += ' ';
+    const botaoApagar = document.createElement('button');
+    botaoApagar.setAttribute('class', 'apagar')
+    botaoApagar.textContent = 'Apagar';
+    li.appendChild(botaoApagar);
+
+    botaoApagar.addEventListener('click', function() {
+        botaoApagar.parentElement.remove();
+
+    // Função oculta criada para quando o botão "apagar" for clicado, apagar o seu elemento pai
+    })
+}
+
+}
+
+meuEscopo();
+

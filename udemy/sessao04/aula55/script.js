6c1b897837fa3bb276619d7c7bfcb1b343779d69
Thiago Wanderley <146901800+thiagowanderley@users.noreply.github.com>
2024-05-07 11:37:57 -0300

    Aula sobre funções imediatas?

---
+++ udemy/sessao04/aula55/script.js
@@ -0,0 +1,13 @@
+// Funções imediatas - IIEF
+
+(function () {
+    function criarNome (nome) {
+        return nome
+    }
+
+    function criarNomeCompleto (sobrenome) {
+        return console.log(criarNome('Thiago') + ' ' + sobrenome)
+    }
+
+    criarNomeCompleto('Wanderley')
+}) ()

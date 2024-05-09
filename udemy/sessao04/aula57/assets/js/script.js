4d02fd5c9bc70c50db844819254a1029f6bc7746
Thiago Wanderley <146901800+thiagowanderley@users.noreply.github.com>
2024-05-09 17:29:02 -0300

    Finalização parcial da calc

--- udemy/sessao04/aula57/assets/js/script.js
+++ udemy/sessao04/aula57/assets/js/script.js
@@ -1,13 +1,37 @@
 function criaCalculadora () {
     return {
         display: document.querySelector('input#display'),
-        clear: document.querySelector('button#clear'),
+        btnClear: document.querySelector('button#clear'),
 
         //Metodos
         inicia() {
             this.cliqueBotoes();
         },
 
+        realizarConta() {
+            let conta = this.display.value;
+
+            try {
+                conta = eval(conta);
+                if (!conta) {
+                    alert('Conta inválida');
+                    return;
+                }
+
+                this.display.value = conta;
+            } catch(e) {
+                
+            }
+        },
+
+        clearDisplay() {
+            this.display.value = '';
+        },
+
+        deletOne() {
+            this.display.value = this.display.value.slice(0, -1);
+        },
+        
         cliqueBotoes() {
             document.addEventListener('click', function(e) {
                 const el = e.target;
@@ -15,6 +39,19 @@ function criaCalculadora () {
                 if (el.classList.contains('btn-num')) {
                     this.btnParaDisplay(el.innerText);
                 }
+
+                if (el.classList.contains('btn-clear')) {
+                    this.clearDisplay();
+                }
+
+                if (el.classList.contains('btn-del')) {
+                    this.deletOne();
+                }
+
+                if (el.classList.contains('btn-eq')) {
+                    this.realizarConta();
+                }
+
             }.bind(this));
         },
 

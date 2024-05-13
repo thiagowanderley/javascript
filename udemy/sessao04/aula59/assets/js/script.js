35a7c47943317c6778f1804564bfa46887b2f400
Thiago Wanderley <146901800+thiagowanderley@users.noreply.github.com>
2024-05-13 17:26:37 -0300

    Tentando entender função construtora

---
+++ udemy/sessao04/aula59/assets/js/script.js
@@ -0,0 +1,29 @@
+function Calculadora () {
+const display = document.getElementById('display');
+
+    this.limparDisplay = () => {
+        this.display.innerText = '';
+    }
+
+    expressarDisplay(valor) {
+        display.innerText += valor
+    }
+
+    this.clickBotoes = () => {
+
+        document.addEventListener('click', (e) => {
+            const ev = e.target;
+
+            if(ev.classList.contains('btn-num')) {
+                expressarDisplay(ev.innerText);
+            }
+
+            if(ev.classList.contains('btn-clear')) {
+                limparDisplay()
+            }
+        })
+    }
+}
+
+const calculadora = new Calculadora()
+

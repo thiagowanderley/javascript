91d02de47e50016c3f2abd866a8528f8535c5056
Thiago Wanderley <146901800+thiagowanderley@users.noreply.github.com>
2024-05-22 12:23:47 -0300

    Finalização da aula sobre filter e inicio de map

--- udemy/sessao05/aula65/filter.js
+++ udemy/sessao05/aula65/filter.js
@@ -15,3 +15,23 @@ console.log(maior10);
 
 const menor10 = x.filter( valor => {return valor < 10})
 console.log(menor10)
+
+//############################################################
+
+const pessoas = [
+    {nome:'Thiago' ,idade: 22},
+    {nome:'Maria' ,idade: 35},
+    {nome:'Eduardo' ,idade: 67},
+    {nome:'Carla' ,idade: 21},
+    {nome:'Wallace' ,idade: 14}
+];
+
+const nomePessoa = pessoas.filter( valor => {return valor.nome.length <= 6}); 
+const menor18 = pessoas.filter( idade18 => idade18.idade < 18);
+const maior50 = pessoas.filter( valor => valor.idade > 50);
+const terminaA = pessoas.filter( valor => valor.nome.endsWith('a'));
+
+console.log(nomePessoa);
+console.log(menor18);
+console.log(maior50);
+console.log(terminaA);
---
+++ udemy/sessao05/aula65/map.js
@@ -0,0 +1,19 @@
+// FILTER, MAP E REDUCE
+
+// MAP
+//Altera valores do array original
+
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+const dobraNumeros = numeros.map( valor => valor*2)
+console.log(dobraNumeros)
+console.log(numeros)
+
+const pessoas = [
+    {nome:'Thiago' ,idade: 22},
+    {nome:'Maria' ,idade: 35},
+    {nome:'Eduardo' ,idade: 67},
+    {nome:'Carla' ,idade: 21},
+    {nome:'Wallace' ,idade: 14}
+];
+
+

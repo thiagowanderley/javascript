608f82559a0184e2d46e79c34cf370d5ca1ed776
Thiago Wanderley <146901800+thiagowanderley@users.noreply.github.com>
2024-06-05 17:32:38 -0300

    Finalização da aula sobre Herança/Delegação e dando inicio no exercicio de validação de CPF

--- udemy/sessao06/aula76/script.js
+++ udemy/sessao06/aula76/script.js
@@ -1,1 +1,40 @@
-// HERANÇA      
+// HERANÇA EM PROTOTYPES
+
+// Produtos -> Camiseta e Caneca
+// Camiseta -> cor ; Caneca -> material 
+
+function Produto (nome, preco) {
+    this.nome = nome;
+    this.preco = preco;
+};
+
+Produto.prototype.aumento = function(valor) {
+    this.preco = this.preco + (this.preco * (valor / 100))
+};
+Produto.prototype.desconto = function(valor) {
+    this.preco = this.preco - (this.preco * (valor / 100))
+};
+
+// Setando o prototype do Produto em Camiseta e Caneca
+function Camiseta (nome, preco, cor) {
+    Produto.call(this, nome, preco, cor)
+    this.cor = cor;
+};
+Camiseta.prototype = Object.create(Produto.prototype);
+Camiseta.prototype.constructor = Camiseta;
+//------------------------------------------------
+function Caneca (nome, preco, material) {
+    Produto.call(this, nome, preco);
+    this.material = material
+};
+Caneca.prototype = Object.create(Produto.prototype);
+Caneca.prototype.constructor = Caneca;
+
+const produto = new Produto('Gen', 10);
+const camiseta = new Camiseta('Regata', 24, 'azul');
+const caneca = new Caneca('Aniversario', 40, 'Porcelana')
+camiseta.desconto(10);
+caneca.desconto(20)
+console.log(camiseta);
+console.log(produto);
+console.log(caneca)

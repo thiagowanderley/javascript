a34dcc22077dabc5959e1caaed9813550d48b699
Thiago Wanderley <146901800+thiagowanderley@users.noreply.github.com>
2024-06-21 12:16:11 -0300

    Finalização parcial - validação de form com class

--- udemy/sessao07/aula85-form/assets/script.js
+++ udemy/sessao07/aula85-form/assets/script.js
@@ -19,9 +19,27 @@ class ValidaForm {
     camposValidos () {
         let valid = true;
 
-        for (let campo of this.formulario.querySelectorAll('.input')) {
-            if (!campo.value) return
+        for (let error of this.formulario.querySelectorAll('.error-text')) {
+            error.remove();
         }
+
+        for (let campo of this.formulario.querySelectorAll('.input')) {
+            const label = campo.previousElementSibling.innerText;
+
+            if (!campo.value) {
+                this.criaError (campo, `O campo "${label}" não pode estar vazio`);
+                valid = false
+            }
+        }
+    }
+
+    criaError (campo, msg) {
+        const newDiv = document.createElement('div');
+        newDiv.innerHTML = msg;
+        newDiv.style.color = 'red';
+        newDiv.style.fontSize = '0.8em'
+        newDiv.classList.add('error-text');
+        campo.insertAdjacentElement('afterend', newDiv);
     }
 
 }

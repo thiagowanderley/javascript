255714abbf7f2b2037619b8d8f11cda5f2c5da07
Thiago Wanderley <146901800+thiagowanderley@users.noreply.github.com>
2024-07-03 17:22:10 -0300

    Realização parcial do exercicio

--- udemy/sessao07/aula85-form/assets/script.js
+++ udemy/sessao07/aula85-form/assets/script.js
@@ -1,3 +1,4 @@
+
 class ValidaForm {
     constructor () {
         this.formulario = document.querySelector('#form')
@@ -12,23 +13,37 @@ class ValidaForm {
 
     handleSubmit (e) {
         e.preventDefault();
+        this.limpaError()
         const camposValidos = this.checaCampos();
     }
 
     checaCampos () {
         let valid = true;
+        const usuario = this.formulario.querySelector('#usuario').value;
 
         for (let campos of this.formulario.querySelectorAll('.input')) {
-            if (typeof campos.value === 'undefined') {
-                this.criaError()
+            if (!campos.value) {
+                this.criaError(campos, `O campo <strong>${campos.previousElementSibling.innerText}</strong> não pode está em branco`);
+                valid = false;
             } 
+        }
+
+        if (this.usuario.lenght < 3 || this.usuario.lenght > 12) {
+            this.criaError(usuario, `Usuario precisa ter entre 3 a 12 caracteres`)
         }
     }
 
     criaError (campo, msg) {
         const div = document.createElement('div');
         div.innerHTML = msg;
-        div.setAttribute('class', 'msg-error ')
+        div.setAttribute('class', 'msg-error');
+        campo.insertAdjacentElement('afterend', div);
+        // div => elemento que vai ser inserido apos o campo
+    }
+
+    limpaError () {
+        const error = document.querySelectorAll('.msg-error');
+        error.forEach( error => error.remove())
     }
 }
 

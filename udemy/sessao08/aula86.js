c8b2d1ebe56ed4f1d77ae86a72a563abc4cf379d
Thiago Wanderley <146901800+thiagowanderley@users.noreply.github.com>
2024-07-05 12:25:46 -0300

    Aprendendo promises

--- udemy/sessao08/aula86.js
+++ udemy/sessao08/aula86.js
@@ -1,9 +1,14 @@
 // PROMISES
 
-function esperaAi (msg, time) {
-    setTimeout(() => {
+function random (min, max) {
+    console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+}
+
+function stringo (msg, time) {
+    setTimeout( () => {
         console.log(msg)
     }, time)
 }
 
-esperaAi('TO AQ', 2000)
+stringo('ola', 3000)
+random(10, 20)

578028c95c4c6ad479784e4b87e33f03a6818118
Thiago Wanderley <146901800+thiagowanderley@users.noreply.github.com>
2024-07-11 12:05:00 -0300

    Finalização da seção 08 e dando inicio a seção 09

--- udemy/sessao08/aula90/assets/js/script.js
+++ udemy/sessao08/aula90/assets/js/script.js
@@ -1,11 +1,39 @@
-const container = document.getElementById('#container');
 
+/*
 fetch('pessoas.json')
-    .then( resposta => resposta.json())
-    .then( json => SeparaEl(json))
-        
+    .then( response => response.json())
+    .then( json => carregaElement(json))
+    .catch( e => console.log(e))
+*/
+axios('pessoas.json') // script importado no html
+    .then(resposta => carregaElement(resposta.data))
 
-function SeparaEl(json) {
-    const nome = json.map( val => val.nome)
-    container.innerText = nome
-}
+ function carregaElement(json) {
+
+    const table = document.createElement('table');
+    
+    for (let pessoas of json) {
+
+        const tr = document.createElement('tr');
+
+        let td1 = document.createElement('td');
+        td1.innerHTML = pessoas.nome;
+        tr.appendChild(td1);
+
+        let td2 = document.createElement('td');
+        td2.innerHTML = pessoas.idade;
+        tr.appendChild(td2);
+
+        let td3 = document.createElement('td');
+        td3.innerHTML = pessoas.salario;
+        tr.appendChild(td3);
+
+        table.appendChild(tr)
+    }
+
+    const divPessoas = document.querySelector('div.pessoas');
+    divPessoas.appendChild(table)
+ }
+    
+
+    

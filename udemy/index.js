4c360934e1b995b2874e71b4c37ae3cc43243383
Thiago Wanderley <146901800+thiagowanderley@users.noreply.github.com>
2024-07-18 09:33:31 -0300

    Test

---
+++ udemy/index.js
@@ -0,0 +1,11 @@
+const num = 3;
+
+const myPromise = new Promise((res, rej) => {
+            
+})
+
+/*
+    configs:
+
+    configurar terminal => Tem um monte de coisa sobre isso no meu canal: https://www.youtube.com/results?search_query=%22zsh%22+%22Ot%C3%A1vio+Miranda%22
+*/
